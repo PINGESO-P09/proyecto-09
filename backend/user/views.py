8f21be4ffc2265b45a1d0ac6e37c8fe78235e25b
Carolina Antillanca <carolina.antillanca@usach.cl>
2024-11-03 22:35:45 -0300

    [Carolina] Creación de usuario (serializers, url y views.py)
    Con esto se completa la creación de usuarios ya que añadí el create (post)

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,8 +2,19 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-class Logout(APIView):
+# Objetivo: Vista crear usuario
+class UserCreateView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Objetivo: Vista cerrar sesion
+class LogoutView(APIView):
     def post(self, request):
         try:
             refresh_token = request.data["refresh_token"]
@@ -12,4 +23,3 @@ class Logout(APIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
