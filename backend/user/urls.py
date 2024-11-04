f4e774fe243e45c8da80f40fa8e945f312d51adb
Carolina Antillanca <carolina.antillanca@usach.cl>
2024-11-03 22:35:45 -0300

    [Carolina] Creación de usuario (serializers, url y views.py)
    Con esto se completa la creación de usuarios ya que añadí el create (post)

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import LogoutView
+from .views import UserCreateView
 
 urlpatterns = [
-    # Obtener token (inicio sesion)
+    # Ruta -> Crear usuario
+    path('create/', UserCreateView.as_view(), name='user_create'),
+    # Ruta -> Obtener token (inicio sesion)
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # Refrescar token
+    # Ruta -> Refrescar token
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Cerrar sesion
+    # Ruta -> Cerrar sesion
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
