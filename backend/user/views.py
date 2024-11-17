9b5f8e1e8c45702a55ff109d0d62191be50e4adf
Andres Zelaya <andres.zelaya@usach.cl>
2024-11-17 18:56:18 -0300

    [Andres] Mejoras en view.py de user

--- backend/user/views.py
+++ backend/user/views.py
@@ -3,9 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import UserSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Objetivo: Vista crear usuario
+@method_decorator(csrf_exempt, name='dispatch') # Deshabilitar CSRF (no recomendado para producción)
 class UserCreateView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -15,13 +18,17 @@ class UserCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# Objetivo: Vista cerrar sesion
+# Objetivo: Vista cerrar sesión
+@method_decorator(csrf_exempt, name='dispatch') # Deshabilitar CSRF (no recomendado para producción)
 class LogoutView(APIView):
     def post(self, request):
         try:
-            refresh_token = request.data["refresh_token"]
+            refresh_token = request.data.get("refresh_token")
+            if not refresh_token:
+                return Response({"error": "Refresh token is required"}, status=status.HTTP_400_BAD_REQUEST)
+
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except Exception as exp:
+            return Response({"error": str(exp)}, status=status.HTTP_400_BAD_REQUEST)
