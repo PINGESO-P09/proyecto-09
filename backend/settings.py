e1ca26a4d644887d08ba36cbbff995981ca37ed8
Carolina Antillanca <carolina.antillanca@usach.cl>
2024-11-20 21:08:02 -0300

    Arreglo static file
    se comentó la linea de static files en settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    #"django.contrib.staticfiles",
     "api",
     "rest_framework",
     "corsheaders",
